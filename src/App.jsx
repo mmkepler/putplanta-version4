9d354d2c3bf6e9b56c718a152a3ed7b94aa2587c
M. Kepler <mmkepler@yahoo.com>
2024-07-20 18:50:52 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>  
+      <h1>Puplanta</h1>
+      
+      <p className="read-the-docs">
+       website goes here
+      </p>
+    </>
+  )
+}
+
+export default App
