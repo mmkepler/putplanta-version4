05773be431dd09ff95580f783d7e02822da58b9b
M. Kepler <mmkepler@yahoo.com>
2024-08-09 09:35:17 -0600

    feat: basic page routing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { withRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
-import Stores from './Stores';
+import Stores from './Stores'
 import Parks from './Parks'
 import Account from './Account'
 import SignIn from './SignIn'
@@ -13,8 +12,24 @@ import ParkInfo from './ParkInfo'
 import StoreInfo from './StoreInfo'
 import Privacy from './Privacy'
 
+
 export default function Main() {
   return (
-    <div>Main</div>
+    <div id='main'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+        <Route path='/stores' element={<Stores/>}/>
+        <Route path='/parks' element={<Parks/>} />
+        <Route path ='/account' element={<Account/>}/>
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='passwordforget' element={<PasswordForgetPage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/parkinfo' element={<ParkInfo/>} />
+        <Route path='/storeinfo' element={<StoreInfo/>} />
+        <Route path='/privacy' element={<Privacy/>} />
+      </Routes>
+      
+    </div>
   )
 }
